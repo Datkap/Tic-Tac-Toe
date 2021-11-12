bccae66a4b3d1fd078200856fa35df54af4001b5
Datkap <k.dzida@datkap.com>
2021-11-12 17:31:07 +0100

    All functions created.  Engine ready for testing.

--- tic-tac-toe_engine.py
+++ tic-tac-toe_engine.py
@@ -17,7 +17,9 @@ def display_board(board):
 
 def select_mark():
     """Asks Player 1 to choose, whether he wants to play 'x' or 'o'. Also gives Player 2 remaining mark."""
-    chosen_mark = input("Choose mark for first player ('x' or 'o'):")
+    player_1_name = input("Please provide name for Player 1:")
+    player_2_name = input("Please provide name for Player 2:")
+    chosen_mark = input(f"{player_1_name} please choose a mark ('x' or 'o'):")
     if chosen_mark == 'x' or chosen_mark == 'o':
         if chosen_mark == 'x':
             player_1 = 'x'
@@ -29,62 +31,81 @@ def select_mark():
         print("Choose between 'x' and 'o' please.")
         select_mark()
 
-    print(f"Player 1 is playing '{player_1}' and Player 2 is playing '{player_2}'.")
-    return player_1, player_2
+    print(f"{player_1_name} is playing '{player_1}' and {player_2_name} is playing '{player_2}'.")
+    return (player_1, player_1_name, player_2, player_2_name)
 
 
+def current_player(move, player_1, player_1_name, player_2, player_2_name):
+    """Determine which player is making a move."""
+    if move%2 == 0:
+        print(f"Current player: {player_1_name}")
+        move += 1
+        return player_1, player_1_name, move
+    else:
+        print(f"Current player: {player_2_name}")
+        move += 1
+        return player_2,player_2_name, move
 
-def get_coordinates_from_player():
+
+def get_coordinates_from_player(board):
     """Ask current player for coordinates where he want to put his mark."""
-    while True:
-        input_text = f"{'Krzyżyk' if mark in ['x', 'X'] else 'Kółko'}: podaj współrzędne w formacie (y,x): "
-        raw_input = input(input_text)
+    raw_input = input("Please specify coordinates for your mark (e.g. A1, C2 etc.):")
+    if len(raw_input) == 2:
+        if raw_input[0].upper() in ['A', 'B', 'C'] and int(raw_input[1]) in [1, 2, 3]:
+            row = int(np.where(board == raw_input[1])[0])
+            column = int(np.where(board == raw_input[0])[1])
+        else:
+            print("Something went wrong...")
+            get_coordinates_from_player(board)
+    else:
+        print("Something went wrong...")
+        get_coordinates_from_player(board)
 
-        if raw_input[0].isnumeric() and raw_input[-1].isnumeric():
-            if int(raw_input[0]) >= 0 and int(raw_input[0]) <= 2 and int(raw_input[-1]) >= 0 and int(
-                    raw_input[-1]) <= 2:
-                break
+    return (row, column)
 
-    return (int(raw_input[0]), int(raw_input[-1]))
-
-
-def put_item_on_board(board, coords: tuple, mark: str):
-    if mark != 'o' and mark != 'x':
+def put_mark_on_board(board, coords, mark):
+    """Puts current players mark on board. If given field is taken"""
+    if board[coords] == '.':
+        board[coords] = mark
+        return board, True
+    else:
+        print("Sorry, this field is already taken. Try different coords.")
         return board, False
-
-    if board[coords] != '.':
-        return board, False
-
-    board[coords] = mark
-
-    return board, True
-
 
 def check_if_game_over(board):
-    for r in board:
-        if all(r == 'x') or all(r == 'o'):
+    """Checks if the game is finished based on current board state."""
+    for row in board[1:, 1:]:
+        if all(row == 'x') or all(row == 'o'):
             return True
 
-    for c in range(board.shape[1]):
-        if all(board[:, c] == 'x') or all(board[:, c] == 'o'):
+    for row in board[1:, 1:].transpose():
+        if all(row == 'x') or all(row == 'o'):
             return True
 
-    if all(board.diagonal() == 'x') or all(board.diagonal() == 'o'):
+    if all(board[1:, 1:].diagonal() == 'x') or all(board[1:, 1:].diagonal() == 'o'):
         return True
 
-    if all(np.fliplr(board).diagonal() == 'x') or all(np.fliplr(board).diagonal() == 'o'):
+    if all(np.fliplr(board[1:, 1:]).diagonal() == 'x') or all(np.fliplr(board[1:, 1:]).diagonal() == 'o'):
         return True
 
     return False
 
-
-def start_tic_tac_toe():
-    is_game_over = False
-    is_success = False
+def play_game():
+    """Starts a new game."""
     board = generate_board()
-    turn = 0
+    display_board(board)
+    player_1, player_1_name, player_2, player_2_name = select_mark()
+    move = 0
+    successful_mark = False
+    is_game_over = False
 
     while is_game_over == False:
-        coords = get_coordinates_from_player()
-        put_item_on_board(board, coords, mark)
-        check_if_game_over(board)
+        display_board(board)
+        player, player_name, move = current_player(move, player_1, player_1_name, player_2, player_2_name)
+        while successful_mark == False:
+            coords = get_coordinates_from_player(board)
+            board, successful_mark = put_mark_on_board(board, coords, player)
+        is_game_over, winner = check_if_game_over(board)
+
+    winner = player_name
+    return f"Game is over. {winner} won the game."
