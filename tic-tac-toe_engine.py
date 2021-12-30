2de3244ed993a581fff9d5a53d235b807f273804
Datkap <k.dzida@datkap.com>
2021-12-30 22:02:54 +0100

    Game works in general. Crashes if coords are out of range and does not include stalemate situation.

--- tic-tac-toe_engine.py
+++ tic-tac-toe_engine.py
@@ -14,30 +14,29 @@ def display_board(board):
     for row in board:
         print(row)
 
-# SELECT_MARK WYMAGA KOREKTY PO NIE PUSZCZA PĘTLI
 
 def select_mark(player_1_name, player_2_name):
     """Asks Player 1 to choose, whether he wants to play 'x' or 'o'. Also gives Player 2 remaining mark."""
     chosen_mark = input(f"{player_1_name} please choose a mark ('x' or 'o'):")
-    while chosen_mark != 'o' or chosen_mark != 'x':
+    while chosen_mark not in ['o', 'x']:
         print("Something went wrong...")
         chosen_mark = input(f"{player_1_name} please choose a mark ('x' or 'o'):")
 
     if chosen_mark == 'x':
-        player_1 = 'x'
-        player_2 = 'o'
-        print(f"{player_1_name} is playing '{player_1}' and {player_2_name} is playing '{player_2}'.")
+        player_1_mark = 'x'
+        player_2_mark = 'o'
+        print(f"{player_1_name} is playing '{player_1_mark}' and {player_2_name} is playing '{player_2_mark}'.")
     else:
-        player_1 = 'o'
-        player_2 = 'x'
-        print(f"{player_1_name} is playing '{player_1}' and {player_2_name} is playing '{player_2}'.")
+        player_1_mark = 'o'
+        player_2_mark = 'x'
+        print(f"{player_1_name} is playing '{player_1_mark}' and {player_2_name} is playing '{player_2_mark}'.")
 
-    return player_1, player_2
+    return player_1_mark, player_2_mark
 
 
 def current_player(move, player_1, player_1_name, player_2, player_2_name):
     """Determine which player is making a move."""
-    if move%2 == 0:
+    if move % 2 == 0:
         print(f"Current player: {player_1_name}")
         move += 1
         return player_1, player_1_name, move
@@ -48,7 +47,7 @@ def current_player(move, player_1, player_1_name, player_2, player_2_name):
 
 
 def get_coordinates_from_player(board):
-    """Ask current player for coordinates where he want to put his mark."""
+    """Ask current player for coordinates where he wants to put his mark."""
     raw_input = input("Please specify coordinates for your mark (e.g. A1, C2 etc.):")
     if len(raw_input) == 2:
         if raw_input[0].upper() in ['A', 'B', 'C'] and int(raw_input[1]) in [1, 2, 3]:
@@ -64,7 +63,7 @@ def get_coordinates_from_player(board):
     return (row, column)
 
 def put_mark_on_board(board, coords, mark):
-    """Puts current players mark on board. If given field is taken"""
+    """Puts current player's mark on board. Informs player if the field is taken and asks for new coords."""
     if board[coords] == '.':
         board[coords] = mark
         return (board, True)
@@ -112,7 +111,3 @@ def play_game():
     winner = player_name
     display_board(board)
     print(f"Game is over. {winner} won the game.")
-
-
-
-# test change for commitment purposes
